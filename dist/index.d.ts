b8033d82e10e3084f31ea2d762825cd40c3eaa22
ivi3d_huoyongfei <huoyongfei@ivi3d.com>
2020-07-10 15:41:40 +0800

    [fixed] change package name

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,4 @@
 /**
-* @Method: Returns a string of the number passed in, but formatted to currency, like $5.00.
-* Accepts a number with implied decimals.
-* @Param {number}
-* @Return {string}
-*/
-export declare function formatToCurrency(args: number): string;
+ * hello world
+ */
+export declare function helloWorld(): string;
--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,6 @@
 /**
-* @Method: Returns a string of the number passed in, but formatted to currency, like $5.00.
-* Accepts a number with implied decimals.
-* @Param {number}
-* @Return {string}
-*/
-export function formatToCurrency(args: number): string {
-    return `$${(args / 100).toFixed(2)}`;
+ * hello world
+ */
+export function helloWorld() {
+    return 'hello world'
 }
